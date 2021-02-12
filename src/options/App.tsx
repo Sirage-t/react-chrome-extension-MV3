55604a086e6bdb7e662d22b9ed1a4f33b896ce81
Sirage-T <Sirage.Tr@gmail.com>
2021-02-13 01:14:24 +1030

    fix formatting

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-  return <div>Options starting page</div>
-}
+  return <div>Options starting page</div>;
+};
 
-export default App
+export default App;
--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-  return <div>Popup starting page</div>
-}
+  return <div>Popup starting page</div>;
+};
 
-export default App
+export default App;
